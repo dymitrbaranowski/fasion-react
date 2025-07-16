ac5bf8a9fcf68e293f9acf1e938da9b60d55b080
dymitrbaranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-07-16 18:52:57 +0300

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/header/Header.jsx";
+
+function App() {
+  const project = "Fashion React - Start";
+  return (
+    <div className="App">
+      <h1>{project}</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__row">
+          <div className="header__logo">Logo</div>
+          <div className="header__nav">Nav</div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
